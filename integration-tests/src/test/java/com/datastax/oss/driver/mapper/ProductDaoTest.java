efe78ec72f39f7587dfcc10f9fef91e842d60391
tomekl007 <tomasz.lelek@datastax.com>
2019-02-26 11:47:36 +0100

    JAVA-2131 use package/classname API

--- integration-tests/src/test/java/com/datastax/oss/driver/mapper/ProductDaoTest.java
+++ integration-tests/src/test/java/com/datastax/oss/driver/mapper/ProductDaoTest.java
@@ -6,6 +6,7 @@ import static com.google.testing.compile.Compiler.javac;
 import com.datastax.oss.driver.internal.mapper.processor.MapperProcessor;
 import com.google.testing.compile.Compilation;
 import com.google.testing.compile.JavaFileObjects;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.tools.StandardLocation;
+
 import org.junit.Test;
 
 public class ProductDaoTest {
@@ -22,23 +24,24 @@ public class ProductDaoTest {
   @Test
   public void should_generate_product_dao_impl() throws IOException {
     // given
-    String FQCN = "com.datastax.oss.driver.mapper.model.inventory.ProductDao";
+    String packageName = "com.datastax.oss.driver.mapper.model.inventory";
+    String FQCNofSource = packageName + ".ProductDao";
     String sourceFileLocation =
         "src/test/java/com/datastax/oss/driver/mapper/model/inventory/ProductDao.java";
-    String generatedSourceFileLocation =
-        "com/datastax/oss/driver/mapper/model/inventory/ProductDao_Impl.java";
+    String outputSourceFileClassName = "ProductDao_Impl.java";
+
 
     // when
     Compilation compilation =
         javac()
             .withProcessors(new MapperProcessor())
             .compile(
-                JavaFileObjects.forSourceLines(FQCN, loadLinesFromSourceFile(sourceFileLocation)));
+                JavaFileObjects.forSourceLines(FQCNofSource, loadLinesFromSourceFile(sourceFileLocation)));
 
     // then
     assertThat(compilation).succeeded();
     assertThat(compilation)
-        .generatedFile(StandardLocation.SOURCE_OUTPUT, generatedSourceFileLocation)
+        .generatedFile(StandardLocation.SOURCE_OUTPUT, packageName, outputSourceFileClassName)
         .hasSourceEquivalentTo(
             JavaFileObjects.forResource("annotation-processor/ProductDao_Impl_Expected.java"));
   }
