269babe17e61f4eb5b2f33f0171c5175a8de0b20
tomekl007 <tomasz.lelek@datastax.com>
2019-02-26 13:00:29 +0100

    JAVA-2131 add annotation processor in the integration-tests submodule

--- integration-tests/src/test/java/com/datastax/oss/driver/mapper/MapperProcessorTestUtil.java
+++ integration-tests/src/test/java/com/datastax/oss/driver/mapper/MapperProcessorTestUtil.java
@@ -21,11 +21,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class MapperProcessorTestUtil {
   private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
   static List<String> loadLinesFromSourceFile(String path) throws IOException {
-    return Files.lines(Paths.get(path), UTF8_CHARSET).collect(Collectors.toList());
+    try (Stream<String> paths = Files.lines(Paths.get(path), UTF8_CHARSET)) {
+      return paths.collect(Collectors.toList());
+    }
   }
 }
