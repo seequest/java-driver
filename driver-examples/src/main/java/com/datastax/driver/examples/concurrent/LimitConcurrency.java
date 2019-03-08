6d89d469df51b7de4798ebd67039d2087b4c766b
tomekl007 <tomasz.lelek@datastax.com>
2019-03-08 13:58:58 +0100

    JAVA-2148 limit concurrency example working

--- driver-examples/src/main/java/com/datastax/driver/examples/concurrent/LimitConcurrency.java
+++ driver-examples/src/main/java/com/datastax/driver/examples/concurrent/LimitConcurrency.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datastax.driver.examples.concurrent;
 
 import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
@@ -12,19 +27,16 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
 public class LimitConcurrency {
   private static final int CONCURRENCY_LEVEL = 32;
   private static final int TOTAL_NUMBER_OF_INSERTS = 10_000;
 
-  static {
-    System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "32");
-  }
-
-  public static void main(String[] args) throws ExecutionException, InterruptedException {
+  public static void main(String[] args) throws InterruptedException {
 
     // The Session is what you use to execute queries. It is thread-safe and should be
     // reused.
@@ -34,8 +46,7 @@ public class LimitConcurrency {
     }
   }
 
-  private static void insertConcurrent(CqlSession session)
-      throws ExecutionException, InterruptedException {
+  private static void insertConcurrent(CqlSession session) throws InterruptedException {
     PreparedStatement pst =
         session.prepare(
             insertInto("examples", "tbl_sample_kv")
@@ -43,31 +54,38 @@ public class LimitConcurrency {
                 .value("value", bindMarker("value"))
                 .build());
 
-    IntStream range = IntStream.range(0, TOTAL_NUMBER_OF_INSERTS);
+    CountDownLatch requestLatch = new CountDownLatch(TOTAL_NUMBER_OF_INSERTS);
+    ExecutorService executor = Executors.newFixedThreadPool(CONCURRENCY_LEVEL);
+
     AtomicInteger insertsCounter = new AtomicInteger();
     Set<String> threads = new HashSet<>();
 
-    range
-        .parallel()
-        .forEach(
-            counter -> {
-              insertsCounter.incrementAndGet();
-              System.out.println("insert from Thread: " + Thread.currentThread().getName());
-              System.out.println("Active threads: " + Thread.activeCount());
-              threads.add(Thread.currentThread().getName());
-              System.out.println(String.format("Value for: %s", counter));
-              CompletableFuture<? extends AsyncResultSet> completableFuture =
-                  session
-                      .executeAsync(
-                          pst.bind()
-                              .set("id", UUID.randomUUID(), UUID.class)
-                              .set("value", String.format("Value for: %s", counter), String.class))
-                      .toCompletableFuture();
-              CompletableFutures.getUninterruptibly(completableFuture);
-            });
+    for (int i = 0; i < TOTAL_NUMBER_OF_INSERTS; i++) {
+      int counter = i;
+      CompletableFuture.supplyAsync(
+          () -> {
+            insertsCounter.incrementAndGet();
+            threads.add(Thread.currentThread().getName());
+            CompletableFuture<? extends AsyncResultSet> completableFuture =
+                session
+                    .executeAsync(
+                        pst.bind()
+                            .set("id", UUID.randomUUID(), UUID.class)
+                            .set("value", String.format("Value for: %s", counter), String.class))
+                    .toCompletableFuture();
+            AsyncResultSet executedRequest =
+                CompletableFutures.getUninterruptibly(completableFuture);
+            requestLatch.countDown();
+            return executedRequest;
+          },
+          executor);
+    }
+    requestLatch.await();
 
-    System.out.println(insertsCounter.get());
-    System.out.println(threads.size());
+    System.out.println(
+        String.format(
+            "Finished executing %s queries with a concurrency level of %s.",
+            insertsCounter.get(), threads.size()));
   }
 
   private static void createSchema(CqlSession session) {
