ce87845f20d7c0a9cb4cc09880e151b00969d294
tomekl007 <tomasz.lelek@datastax.com>
2019-03-11 12:13:09 +0100

    JAVA-2182 replace custom LimitConcurrency example with usage of RequestThrottler

---
+++ driver-examples/src/main/java/com/datastax/driver/examples/concurrent/LimitConcurrencyRequestThrottler.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.driver.examples.concurrent;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.internal.core.session.throttling.ConcurrencyLimitingRequestThrottler;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Creates a keyspace and tables, and loads data using Async API into them.
+ *
+ * <p>This example makes usage of a {@link CqlSession#executeAsync(String)} method, which is
+ * responsible for executing requests in a non-blocking way. It uses {@link
+ * ConcurrencyLimitingRequestThrottler} to limit number of concurrent request to 32. It uses
+ * advanced.throttler configuration to limit async concurrency (see application.conf)
+ *
+ * <p>Preconditions: - a Cassandra session is running and accessible through the contacts points
+ * identified by basic.contact-points (see application.conf)
+ *
+ * <p>Side effects: - creates a new keyspace "examples" in the session. If a keyspace with this name
+ * already exists, it will be reused; - creates a table "examples.tbl_sample_kv". If it exist
+ * already, it will be reused; - inserts a TOTAL_NUMBER_OF_INSERTS of rows into the table.
+ *
+ * @see <a href="http://datastax.github.io/java-driver/manual/">Java driver online manual</a>
+ */
+public class LimitConcurrencyRequestThrottler {
+  private static final int TOTAL_NUMBER_OF_INSERTS = 10_000;
+
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+    try (CqlSession session = new CqlSessionBuilder().build()) {
+      createSchema(session);
+      insertConcurrent(session);
+    }
+  }
+
+  private static void insertConcurrent(CqlSession session)
+      throws InterruptedException, ExecutionException {
+    PreparedStatement pst =
+        session.prepare(
+            insertInto("examples", "tbl_sample_kv")
+                .value("id", bindMarker("id"))
+                .value("value", bindMarker("value"))
+                .build());
+    // Create list of pending CompletableFutures. We will add every operation returned from
+    // executeAsync
+    // Next, we will wait for completion of all TOTAL_NUMBER_OF_INSERTS
+    List<CompletableFuture> pending = new ArrayList<>();
+
+    // For every i we will insert a record to db
+    for (int i = 0; i < TOTAL_NUMBER_OF_INSERTS; i++) {
+      pending.add(
+          session
+              .executeAsync(
+                  pst.bind()
+                      .set("id", UUID.randomUUID(), UUID.class)
+                      .set("value", String.format("Value for: %s", i), String.class))
+              // Transform CompletionState toCompletableFuture to be able to wait for execution of
+              // all using CompletableFuture.allOf
+              .toCompletableFuture());
+    }
+
+    // Wait for completion of all TOTAL_NUMBER_OF_INSERTS pending requests
+    CompletableFuture.allOf(pending.toArray(new CompletableFuture[pending.size()])).get();
+
+    System.out.println(
+        String.format(
+            "Finished executing %s queries with a concurrency level of 32.", pending.size()));
+  }
+
+  private static void createSchema(CqlSession session) {
+    session.execute(
+        "CREATE KEYSPACE IF NOT EXISTS examples "
+            + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+
+    session.execute(
+        "CREATE TABLE IF NOT EXISTS examples.tbl_sample_kv (id uuid, value text, PRIMARY KEY (id))");
+  }
+}
